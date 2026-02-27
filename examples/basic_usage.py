32db042cc26c9e90e09e9aaada03230ef93f471f
Harry Carey <harry.carey95@gmail.com>
2026-02-27 18:05:31 +0100

    Implement basic usage example in basic_usage.py

---
+++ examples/basic_usage.py
@@ -0,0 +1,27 @@
+import numpy as np
+from brainglobe_data_api import query_datasets, BrainGlobeDataset
+from brainglobe_atlasapi import BrainGlobeAtlas
+# 1. Discover the dataset using openMINDS metadata
+)
+"""
+we can query using openminds metadata. Relevant fields we might start with are
+mentioned in our supported-metadata.md file. 
+
+"""
+results = query_datasets(
+    technique="viral tracing", 
+    anatomical_target="MOp"
+
+#returns a dataframe with matching datasets and the associated metadata
+target_dataset_name = results.iloc[0]["unique_name"]
+
+# 2. Load the dataset (downloads manifest, prepares Zarr connection)
+dataset = BrainGlobeDataset(target_dataset_name)
+
+# 3. Retrieve the associated BrainGlobeAtlas
+atlas_name = dataset.atlas_space # -> allen_mouse
+dataset_resolution = dataset.volume_resolution_um
+atlas = BrainGlobeAtlas(f"{atlas_name}_{dataset_resolution}_um")
+
+
+
